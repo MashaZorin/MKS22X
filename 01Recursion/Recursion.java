c8ab3a3678353438b3b146c7544dc7b77a67785f
Masha Zorin <masha.zorin@gmail.com>
2017-02-06 20:48:33 -0500

    sqrt method written and tested

---
+++ 01Recursion/Recursion.java
@@ -0,0 +1,26 @@
+public class Recursion{
+    public static String name(){
+	return "Zorin,Maria";
+    }
+
+    public static double sqrt(double n){
+	return guessSqrt(n, n / 2);
+    }
+
+    public static double guessSqrt(double n, double guess){
+	if ((n - guess * guess) / n * 100 < 0.001 && (n - guess * guess) / n * 100 > -0.001){
+	    return guess;
+	}
+	else{
+	    double better_guess = (n / guess + guess) / 2;
+	    return guessSqrt(n, better_guess);
+	}
+    }
+
+    public static void main(String[]args){
+	System.out.println(sqrt(100));
+	System.out.println(sqrt(4));
+	System.out.println(sqrt(1000));
+	System.out.println(sqrt(10000));
+    }
+}
