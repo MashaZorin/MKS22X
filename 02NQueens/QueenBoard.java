b3e382fa675ae001d3f2a33ed79e73dc95ba20b8
Masha Zorin <masha.zorin@gmail.com>
2017-02-10 03:33:33 -0500

    wrote recursive solve function, but not tested (havent tried compiling yet)

---
+++ 02NQueens/QueenBoard.java
@@ -0,0 +1,91 @@
+public class QueenBoard{
+    
+    private int[][]board;
+    private int solutionCount;
+
+    
+
+    public QueenBoard(int size){
+	board = new int[size][size];
+    }
+
+    /**
+     *precondition: board is filled with 0's only.
+     *@return false when the board is not solveable. true otherwise.
+     *postcondition: 
+     *if false: board is still filled with 0's
+     *if true: board is filled with the 
+     *final configuration of the board after adding 
+     *all n queens. Uses solveH
+     */
+    public boolean solve()
+    {
+	return solveH(0);
+    }
+
+    private boolean solveH(int col){
+	if (col < board.size){
+	    for (int row = 0; row < board[].size; row ++){
+		if (board[col][row] == 0){
+		    board[col][row] = 1;
+		    for (int i = 0; i < board.size * board[].size; i ++){
+			c = i / board.size;
+			r = i % board.size;
+			if (c != col && r == row){
+			    board[c][r] -= 1;
+			}
+			else if (c == col && r != row){
+			    board[c][r] -= 1;
+			}
+			else if (row - r == col - c){
+			    board[c][r] -= 1;
+			}
+		    }
+		    if (!solveH(col + 1)){
+			board[col][row] = -1;
+			for (int i = 0; i < board.size * board[].size; i ++){
+			    c = i / board.size;
+			    r = i % board.size;
+			    if (c != col && r == row){
+				board[c][r] += 1;
+			    }
+			    else if (c == col && r != row){
+				board[c][r] += 1;
+			    }
+			    else if (row - r == col - c && r != row){
+				board[c][r] += 1;
+			    }
+			}
+			return solveH(col);
+		    }
+		    else{
+			return true;
+		    }
+		}
+	    }
+	    return false;
+	}
+	else {
+	    return true;
+	}
+    }
+
+    /**
+     *@return the number of solutions found, or -1 if the board was never solved.
+
+     *The board should be reset after this is run.    
+
+     */
+
+    public int getSolutionCount(){
+    	return -1;
+    }
+
+    /**toString
+     *and all nunbers that represent queens are replaced with 'Q' 
+     *all others are displayed as underscores '_'
+     */
+    public String toString(){
+    	return "";
+    }
+}
