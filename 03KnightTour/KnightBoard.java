9a934b500f2c90308202b50838acda806e85db6c
Masha Zorin <masha.zorin@gmail.com>
2017-02-18 10:36:39 -0500

    wrote toString and solve, compiles, but untested

--- 03KnightTour/KnightBoard.java
+++ 03KnightTour/KnightBoard.java
@@ -11,10 +11,11 @@ public class KnightBoard{
     }
 
     public void solve(){
+	solveH(0,0,1);
     }
 
     private boolean solveH(int row ,int col, int level){
-	if (level < board.length * board.length[0]){
+	if (level < board.length * board[0].length){
 	    if (isGoodSpot(row, col)){
 		board[row][col] = level;
 		return solveH(row + 2, col + 1, level + 1) ||
@@ -38,11 +39,26 @@ public class KnightBoard{
     private boolean isGoodSpot(int row, int col){
 	return row < board.length &&
 	    row >= 0              &&
-	    col < board[].length  &&
+	    col < board[0].length &&
 	    col >= 0              &&
 	    board[row][col] == 0;
     }
 
     public String toString(){
+	String boardStr = "";
+	for (int r = 0; r < board.length; r ++){
+	    for (int c = 0; c < board[0].length; c ++){
+		for (int i = 0; i < ("" + (board.length * board[0].length)).length(); i ++){
+		    if (("" + board[r][c]).length() == ("" + (board.length * board[0].length)).length() - i){
+			for (int j = 0; j < i; j ++){
+			    boardStr += " ";
+			}
+			boardStr += board[r][c];
+		    }
+		}
+	    }
+	    boardStr += "\n";
+	}
+	return boardStr;
     }
 }
