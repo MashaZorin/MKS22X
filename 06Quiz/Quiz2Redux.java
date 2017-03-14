54b7fb1caef53e7fcb8d3886f813b486d5ada449
Masha Zorin <masha.zorin@gmail.com>
2017-03-13 21:20:15 -0400

    completed - didn't use recursion, hope its ok

--- 06Quiz/Quiz2Redux.java
+++ 06Quiz/Quiz2Redux.java
@@ -7,13 +7,25 @@ public class Quiz2Redux{
    */
   public static ArrayList<String> combinations(String s){
       ArrayList<String>words = new ArrayList<String>();
-      help( words , /*fill this in with more */);
+      help( words , s /*fill this in with more */);
       Collections.sort(words);
       return words;
   }
   
-  private static void help( ArrayList<String> words, 
-                             /*fill this in with more arguments*/ ){
-   /*METHOD TO BE WRITTEN BY YOU.*/ 
-  }
+    private static void help( ArrayList<String> words, String s/*fill this in with more arguments*/ ){
+   /*METHOD TO BE WRITTEN BY YOU.*/
+	words.add("");
+	for (int strIndex = 0; strIndex < s.length(); strIndex ++){
+	    int size = words.size();
+	    char letter = s.charAt(strIndex);
+	    for (int aryIndex = 0; aryIndex < size; aryIndex ++){
+		words.add(words.get(aryIndex) + letter);
+	    }
+	}
+    }
+
+    public static void main(String[]args){
+	System.out.println(combinations("abc"));
+	System.out.println(combinations("wxof"));
+    }
 }
