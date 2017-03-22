4c298fb9ef8adeb7b648bd927fd6a03adf736825
Masha Zorin <masha.zorin@gmail.com>
2017-03-22 12:37:03 -0400

    test cases

--- 07Quick/Quick.java
+++ 07Quick/Quick.java
@@ -1,3 +1,4 @@
+import java.util.Random;
 public class Quick{
     public static void quicksort(int[]ary){
 	int lo = 0;
@@ -52,6 +53,16 @@ public class Quick{
     }
 
     public static void main(String[]args){
+	Random rand  = new Random();
+	for (int i = 0; i < 20; i ++){
+	    int[]ary = new int[i];
+	    for (int j = 0; j < i; j ++){
+		ary[j] = rand.nextInt(i);
+	    }
+	    printAry(ary);
+	    quicksort(ary);
+	    printAry(ary);
+	}
 	int[]ary = {3,3,3,2,6,7,4,1,0,0,9,4,5,8,0,5};
 	printAry(ary);
 	quicksort(ary);
