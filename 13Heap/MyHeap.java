4371e5454c44c7606ef931faf070430008271367
Masha Zorin <masha.zorin@gmail.com>
2017-05-04 12:02:31 -0400

    wrote method to double size of array

--- 13Heap/MyHeap.java
+++ 13Heap/MyHeap.java
@@ -14,4 +14,27 @@ public class MyHeap{
 	heap = new String[1];
 	size = 0;
     }
+
+    public void add(){
+    }
+
+    public String remove(){
+    }
+
+    public String peek(){
+    }
+
+    private void pushUp(){
+    }
+
+    private void pushDown(){
+    }
+
+    private void doubleSize(){
+	String[] temp = new String[heap.length * 2];
+	for (int i = 1; i < size; i ++){
+	    temp[i] = heap[i];
+	}
+	heap = temp;
+    }
 }
