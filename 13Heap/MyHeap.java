5fa867dec5bc359e00abe79c60d8338c36fca39d
Masha Zorin <masha.zorin@gmail.com>
2017-05-04 12:25:31 -0400

    wrote pushUp and add

--- 13Heap/MyHeap.java
+++ 13Heap/MyHeap.java
@@ -1,33 +1,55 @@
 public class MyHeap{
     private String[] heap;
-    private boolean maxOrMin; // true is max, min is false
+    private boolean max; // true is max, min is false
     private int size;
     
     public MyHeap(){
-	maxOrMin = true;
+	max = true;
 	heap = new String[1];
 	size = 0;
     }
 
     public MyHeap(boolean m){
-	maxOrMin = m;
+	max = m;
 	heap = new String[1];
 	size = 0;
     }
 
-    public void add(){
+    public void add(String s){
+	if (size + 1 == heap.length){
+	    doubleSize();
+	}
+	heap[size + 1] = s;
+	int index = size + 1;
+	if (max){
+	    while (index != 1 && heap[index] > heap[index / 2]){
+		pushUp(index);
+		index = index / 2;
+	    }
+	}
+	else{
+	    while (index != 1 && heap[index] < heap[index 1 2]){
+		pushUp(index);
+		index = index / 2;
+	    }
+	}
+	size ++;
     }
 
     public String remove(){
     }
 
     public String peek(){
+	return heap[i];
     }
 
     private void pushUp(){
     }
 
-    private void pushDown(){
+    private void pushDown(int index){
+	int temp = heap[index];
+	heap[index] = heap[index / 2];
+	heap[index / 2] = temp;
     }
 
     private void doubleSize(){
