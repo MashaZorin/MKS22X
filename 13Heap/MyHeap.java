3bfc79c00210dabc0f83535330a7a31deea95c23
Masha Zorin <masha.zorin@gmail.com>
2017-05-09 17:03:24 -0400

    retrying

--- 13Heap/MyHeap.java
+++ 13Heap/MyHeap.java
@@ -15,16 +15,21 @@ public class MyHeap{
 	size = 0;
     }
 
+    /*
     public void add(String s){
 	if (size + 1 == heap.length){
 	    doubleSize();
 	}
 	heap[size + 1] = s;
 	int index = size + 1;
+	System.out.println(index);
+	System.out.println(index / 2);
+	System.out.println(heap[index / 2]);
 	if (max){
 	    while (index > 1 && heap[index].compareTo(heap[index / 2]) > 0){
 		pushDown(index);
 		index = index / 2;
+		System.out.println(index);
 	    }
 	}
 	else{
@@ -33,6 +38,7 @@ public class MyHeap{
 		index = index / 2;
 	    }
 	}
+	System.out.println(this);
 	size ++;
     }
 
@@ -41,16 +47,24 @@ public class MyHeap{
 	heap[1] = heap[size];
 	heap[size] = null;
 	size --;
+	int index = 1;
 	while (index * 2 <= size){
 	    index = pushUp(index);
 	}
 	return old;
     }
-
+    */
+    
     public String peek(){
-	return heap[i];
+	if (size > 0){
+	    return heap[1];
+	}
+	else{
+	    return "empty heap";
+	}
     }
 
+    /*
     private int pushUp(int index){
 	int left = index * 2;
 	int right = left + 1;
@@ -96,10 +110,11 @@ public class MyHeap{
     }
 
     private void pushDown(int index){
-	int temp = heap[index];
+	String temp = heap[index];
 	heap[index] = heap[index / 2];
 	heap[index / 2] = temp;
     }
+    */
 
     private void doubleSize(){
 	String[] temp = new String[heap.length * 2];
@@ -111,12 +126,29 @@ public class MyHeap{
 
     public String toString(){
 	String str = "";
-	for (int i = 1; i < size; i = i * 2){
-	    for (j = i; j < i * 2; j ++){
+	for (int i = 1; i <= size; i = i * 2){
+	    for (int j = i; j < i * 2; j ++){
 		str += heap[j] + " ";
 	    }
 	    str += "\n";
 	}
 	return str;
+    }
+
+    //retry of add, remove, pushUp, and pushDown
+    //add uses pushUp, remove uses pushDown
+    //pushUp checks parent
+    //pushDown checks children
+
+    public void add(String s){
+    }
+
+    public String remove(){
+    }
+
+    private void pushUp(){
+    }
+
+    private void pushDown(){
     }
 }
